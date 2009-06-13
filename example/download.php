7263c7214a31651c21ea02b0301b928bb6ff7c2f
Andrew Heiss <andrewheiss@gmail.com>
2009-06-13 11:29:30 +0300

    Minor edits to documentation

--- example/download.php
+++ example/download.php
@@ -32,19 +32,25 @@
 		$pdf_lastname = $data['lastname'];
 		$pdf_email = $data['email'];
 
-		// $fdf_data_strings associates the names of the PDF form fields to the PHP variables you just set above. In order to work correclty the PDF form field name has to be exact. PDFs made in Acrobat generally have simpler names - just the name you assigned to the field. PDFs made in LiveCycle Designer nest their forms in other random page elements, creating a long and hairy field name. You can use pdftk to discover the real names of your PDF form fields. Just run "pdftk form.pdf dump_data_fields > form-fields.txt" to generate a report.
+		// $fdf_data_strings associates the names of the PDF form fields to the PHP variables you just set above. In order to work correctly the PDF form field name has to be exact. PDFs made in Acrobat generally have simpler names - just the name you assigned to the field. PDFs made in LiveCycle Designer nest their forms in other random page elements, creating a long and hairy field name. You can use pdftk to discover the real names of your PDF form fields: run "pdftk form.pdf dump_data_fields > form-fields.txt" to generate a report.
 
-		// This is an example of field names from a PDF created in LiveCycle:
-		//$fdf_data_strings= array('form1[0].#subform[0].#area[0].LastName[0]' => $pdf_lastname,  'form1[0].#subform[0].#area[0].FirstName[0]' => $pdf_firstname, 'form1[0].#subform[0].#area[0].EMail[0]' => $pdf_email, );
+		// Example of field names from a PDF created in LiveCycle:
+		// $fdf_data_strings= array('form1[0].#subform[0].#area[0].LastName[0]' => $pdf_lastname,  'form1[0].#subform[0].#area[0].FirstName[0]' => $pdf_firstname, 'form1[0].#subform[0].#area[0].EMail[0]' => $pdf_email, );
 		$fdf_data_strings= array('firstname' => $pdf_firstname,  'lastname' => $pdf_lastname, 'email' => $pdf_email);
 		
 		// See the documentation of pdftk-php.php for more explanation of these other variables.
 		
 		// Used for radio buttons and check boxes
-		// Example: with $pdf_checkbox1, 2, 3, etc equal to Yes or Off, $fdf_data_names = array('checkbox1' => $pdf_checkbox1,'checkbox2' => $pdf_checkbox2,'checkbox3' => $pdf_checkbox3,'checkbox4' => $pdf_checkbox4); 
-		$fdf_data_names = array(); 
+		// Example: (For check boxes options are Yes and Off)
+		// $pdf_checkbox1 = "Yes";
+		// $pdf_checkbox2 = "Off";
+		// $pdf_checkbox3 = "Yes";
+		// $fdf_data_names = array('checkbox1' => $pdf_checkbox1,'checkbox2' => $pdf_checkbox2,'checkbox3' => $pdf_checkbox3,'checkbox4' => $pdf_checkbox4); 
+		$fdf_data_names = array(); // Leave empty if there are no radio buttons or check boxes
+		
 		$fields_hidden = array(); // Used to hide form fields
-		$fields_readonly = array(); // Used to make fields read only - however, flattening the output in with pdftk will in effect make every field read only. If you don't want a flattened pdf and still want some read only fields, use this variable and remove the flatten flag near line 70 in pdftk-php.php
+		
+		$fields_readonly = array(); // Used to make fields read only - however, flattening the output with pdftk will in effect make every field read only. If you don't want a flattened pdf and still want some read only fields, use this variable and remove the flatten flag near line 70 in pdftk-php.php
 		
 		// Take each REQUEST value and clean it up for fdf creation
 		foreach( $_REQUEST as $key => $value ) {
